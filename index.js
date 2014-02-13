6c4bc9bdd2a92f09edee69c9534ac845ea6b63c4
Calvin Metcalf <cmetcalf@appgeo.com>
2014-02-13 14:35:57 -0500

    more options

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var es = require('event-stream');
 var mbtiles = require('mbtiles');
 var mkdirp = require('mkdirp');
 
-module.exports = function(inTile, outPath, max, callback) {
+module.exports = function(inTile, outPath, max, mbutilMode, callback) {
   if (outPath[0] !== '/') {
     outPath = outPath + '/';
   }
+  var infoName = typeof mbutilMode !== 'undefined' ? 'metadata.json' : "info.json";
   max = parseInt(max, 10);
   var current = 0;
-
   new mbtiles(inTile, function(err, tiles) {
     if (err) {
       return callback(err);
@@ -22,25 +22,30 @@ module.exports = function(inTile, outPath, max, callback) {
       }
       var writing = tileStream.pipe(es.map(function (data, cb) {
         current++;
-        if (current>=max) {
+        if (current >= max) {
           tileStream.pause();
         }
-        var path;
+        var path, outData;
         if (data.tile || data.grid) {
           path = outPath + data.z + '/' + data.x + '/';
           mkdirp(path, function (err) {
             if (err) {
               return cb(err);
             }
-            var fullpath, outdata;
+            var fullpath, outdata, options = {};
             if (data.tile) {
-              fullpath = path+data.y+'.png';
+              fullpath = path + data.y + '.png';
               outdata = data.tile;
             } else if (data.grid) {
-              fullpath = path+data.y+".grid.json";
-              outdata = JSON.stringify(data.grid); 
+              fullpath = path + data.y + ".grid.json";
+               if (mbutilMode) {
+                outdata = 'typeof '+ mbutilMode + " === 'function' && " + mbutilMode + '(' + JSON.stringify(data) + ');';
+              } else {
+                outdata = JSON.stringify(data);
+              }
+              options.encoding = 'utf8';
             }
-            fs.writeFile(fullpath, outdata, function (err) {
+            fs.writeFile(fullpath, outdata, options, function (err) {
               if (err) {
                 return cb(err);
               }
@@ -51,7 +56,7 @@ module.exports = function(inTile, outPath, max, callback) {
             });
           });
         } else if (data.name) {
-          fs.writeFile(outPath+"info.json", JSON.stringify(data), {encoding: 'utf8'}, cb);
+          fs.writeFile(outPath + infoName, JSON.stringify(data), {encoding: 'utf8'}, cb);
         }
       }));
       writing.on('error', callback);
--- readme.md
+++ readme.md
@@ -11,9 +11,10 @@ Usage: mbliberator [options] mbtiles outpath
 
   Options:
 
-    -h, --help          output usage information
-    -V, --version       output the version number
-    -p, --parallel [n]  max number of parallel writes
+    -h, --help               output usage information
+    -V, --version            output the version number
+    -p, --parallel <n>       max number of parallel writes
+    -m, --mbutil [callback]  mbutil mode, also used to set the grid callback for mbutil mode, if omited defaults to "grid", set to "" to turn off jsonp
 ```
 
 TODO
